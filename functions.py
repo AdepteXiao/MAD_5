a7a0b816ec10ae72243cfb83afe7aa770e76d90d
ArinaD <ademkhina@list.ru>
2023-05-08 22:08:57 +0700

    fin

--- functions.py
+++ functions.py
@@ -16,6 +16,7 @@ import warnings
 def c_plots(est, name, clfs, x_tr, y_tr, x_te, y_te):
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(16, 5))
     fig.suptitle(f"Classifier params: {name}")
+    best_scores = {}
 
     for ax, (clf_name, clf) in zip(axs.flatten(), clfs.items()):
         n_range = range(1, 100)
@@ -28,17 +29,23 @@ def c_plots(est, name, clfs, x_tr, y_tr, x_te, y_te):
                 roc_auc_score(y_tr, model.predict_proba(x_tr)[:, 1]))
             test_vals.append(
                 roc_auc_score(y_te, model.predict_proba(x_te)[:, 1]))
+        best_scores[clf_name] = max(test_vals)
         ax.plot(n_range, train_vals, label="train_score")
         ax.plot(n_range, test_vals, label="test_score")
         ax.set_xlabel("n_estimators")
         ax.set_ylabel("Score")
         ax.set_title(clf_name)
         ax.legend()
+    best_score = max(best_scores.values())
+    rev_dict = {v: k for k, v in best_scores.items()}
+    key = rev_dict[best_score]
+    return best_scores[key], best_score
 
 
 def r_plots(est, name, clfs, x_tr, y_tr, x_te, y_te):
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(16, 5))
     fig.suptitle(f"Regression params: {name}")
+    best_scores = {}
 
     for ax, (clf_name, clf) in zip(axs.flatten(), clfs.items()):
         n_range = range(1, 100)
@@ -51,12 +58,17 @@ def r_plots(est, name, clfs, x_tr, y_tr, x_te, y_te):
                 model.score(x_tr, y_tr))
             test_vals.append(
                 model.score(x_te, y_te))
+        best_scores[clf_name] = max(test_vals)
         ax.plot(n_range, train_vals, label="train_score")
         ax.plot(n_range, test_vals, label="test_score")
         ax.set_xlabel("n_estimators")
         ax.set_ylabel("Score")
         ax.set_title(clf_name)
         ax.legend()
+    best_score = max(best_scores.values())
+    rev_dict = {v: k for k, v in best_scores.items()}
+    key = rev_dict[best_score]
+    return best_scores[key], best_score
 
 
 
